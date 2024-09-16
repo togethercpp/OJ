71eb764f1c247a51260cd4054898248fdf74b242
tqviet <vtqktvn123@gmail.com>
2024-09-16 16:13:29 +0700

    change name

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -68,9 +68,6 @@ VNOJ_HOMEPAGE_TOP_USERS_COUNT = 5
 
 VNOJ_DISPLAY_RANKS = (
     ('user', _('Normal User')),
-    ('setter', _('Problem Setter')),
-    ('daor', _('Bedao Team')),
-    ('staff', _('Staff')),
     ('banned', _('Banned User')),
     ('admin', _('Admin')),
     ('teacher', _('Teacher')),
