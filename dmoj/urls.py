3507f052544bbf73e3278573f8c4a70942d44fef
tqviet <vtqktvn123@gmail.com>
2024-09-18 15:15:52 +0700

    fix

--- dmoj/urls.py
+++ dmoj/urls.py
@@ -422,10 +422,10 @@ urlpatterns = [
 
     path('magazine/', MagazinePage.as_view(), name='magazine'),
     path(r'^import_users/', include([
-        path(r'^$', user.ImportUsersView.as_view(), name='import_users'),
-        path(r'post_file/$', user.import_users_post_file, name='import_users_post_file'),
-        path(r'submit/$', user.import_users_submit, name='import_users_submit'),
-        path(r'sample/$', user.sample_import_users, name='import_users_sample')
+        path(r'', user.ImportUsersView.as_view(), name='import_users'),
+        path(r'post_file/', user.import_users_post_file, name='import_users_post_file'),
+        path(r'submit/', user.import_users_submit, name='import_users_submit'),
+        path(r'sample/', user.sample_import_users, name='import_users_sample')
     ])),
 ]
 
